863323faa14954ce561fa26422cf167d862f3a76
pufaqi <32298636+pufaqi@users.noreply.github.com>
2019-02-21 11:56:08 +0800

    Update PrintHelloWorld.java

--- src/main/java/com/zzw/helloworld/PrintHelloWorld.java
+++ src/main/java/com/zzw/helloworld/PrintHelloWorld.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class PrintHelloWorld {
-    @RequestMapping("/helloworld")
+    @RequestMapping("/fagelaile")
     @ResponseBody
     public String test() {
         return "hello world!";
